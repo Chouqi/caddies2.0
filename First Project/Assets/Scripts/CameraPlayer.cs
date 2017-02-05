4a8ec341273cd914b3871e1a05aa4c876b581e19
Chouqi <ines.jerome.chouqi@gmail.com>
2017-02-06 00:07:49 +0100

    go
    gogogogo

---
+++ First Project/Assets/Scripts/CameraPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayer : MonoBehaviour 
+{
+
+	public Transform target; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Vector3 targetPosition = transform.position; 
+		targetPosition.x = target.position.x;
+
+		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime ); 
+
+		//transform.position = new Vector3 (target.position.x, transform.position.y, transform.position.z); 
+	}
+}
+	
---
+++ First Project/Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Player : MonoBehaviour 
+{
+	private Rigidbody2D body; 
+	public float speed; 
+	public float jumpSpeed;
+	public Transform groundCheck; 
+	public bool isGrounded; 
+	public int coinAmount; 
+	public Text score; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		body = GetComponent <Rigidbody2D> (); 
+	}
+
+	//
+	void Update ()
+	{
+		if ( isGrounded == true && Input.GetButtonDown ("Jump")) 
+		{
+			body.velocity = new Vector2 ( body.velocity.x, jumpSpeed );
+		}
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if ( Physics2D.OverlapPoint (groundCheck.position) == null ) 
+		{
+			isGrounded = false; 
+		}
+		else 
+		{
+			isGrounded = true; 
+		}
+//		float horizontal = Input.GetAxisRaw ("Horizontal");
+		float horizontal = Input.GetAxis ("Horizontal");
+		body.velocity = new Vector2 ( horizontal * speed, body.velocity.y);
+	}
+
+	void OnTriggerEnter2D ( Collider2D other )
+	{
+		Destroy (other.gameObject); 
+
+		coinAmount++; 
+		score.text = "Score : " + coinAmount.ToString ();
+	}
+
+}
